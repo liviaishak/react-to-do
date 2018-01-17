0b28130ab49973ca81b90af8da329497cbbb3423
Livia Ishak <liviaishak1@gmail.com>
2018-01-17 10:30:07 -0800

    Assignment8 completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,6 +5,7 @@ class ToDo extends Component {
     return (
       <li>
         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <button onClick={ this.props.deleteToDo }> Delete</button>
         <span>{ this.props.description }</span>
       </li>
     );
